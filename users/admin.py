28beafa3a19f440cf5879ae4d3cc6116ff700b86
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-22 04:44:08 +0000

    Register the user model on the admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "username",
+        "email",
+        "date_joined",
+        "last_login",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "invigilator",
+    )
+    list_filter = (
+        "last_login",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "date_joined",
+        "invigilator",
+    )
+    raw_id_fields = ("groups", "user_permissions")
+    readonly_fields = ("password", "date_joined", "last_login")
