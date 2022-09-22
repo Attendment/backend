e64d637435c2d3224183492dbf0496062784b4e9
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-22 04:43:57 +0000

    Modify fields on User model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+import uuid
 
 
 class User(AbstractUser):
-    pass
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
+    invigilator = models.BooleanField(
+        verbose_name=_("Invigilator"),
+        help_text="Is this user an invigilator?",
+        default=False,  # type: ignore
+    )
+
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.get_full_name()}"
