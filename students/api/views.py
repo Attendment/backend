34a3630662404346c98a4a0b56e0d6c9874f23f2
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-24 13:55:54 +0000

    Add views for the room and exam api endpoints

--- students/api/views.py
+++ students/api/views.py
@@ -1,26 +1,53 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-from students.api.serializers import ProgrammeSerializer, StudentSerializer
-from students.models import Programme, Student
+from students.api.serializers import (
+    ExamSerializer,
+    ProgrammeSerializer,
+    RoomSerializer,
+    StudentSerializer,
+)
+from students.models import Exam, Programme, Student, Room
 
 
 class StudentListCreateAPIView(ListCreateAPIView):
     serializer_class = StudentSerializer
-    queryset = Student.objects.all()
+    queryset = Student.objects.all()  # type: ignore
 
 
 class StudentRetrieveUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = StudentSerializer
-    queryset = Student.objects.all()
+    queryset = Student.objects.all()  # type: ignore
     lookup_field = "id"
 
 
 class ProgrammeListCreateAPIView(ListCreateAPIView):
     serializer_class = ProgrammeSerializer
-    queryset = Programme.objects.all()
+    queryset = Programme.objects.all()  # type: ignore
 
 
 class ProgrammeRetrieveUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = ProgrammeSerializer
-    queryset = Programme.objects.all()
+    queryset = Programme.objects.all()  # type: ignore
+    lookup_field = "id"
+
+
+class RoomListCreateAPIView(ListCreateAPIView):
+    serializer_class = RoomSerializer
+    queryset = Room.objects.all()  # type: ignore
+
+
+class RoomRetrieveUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = RoomSerializer
+    queryset = Room.objects.all()  # type: ignore
+    lookup_field = "id"
+
+
+class ExamListCreateAPIView(ListCreateAPIView):
+    serializer_class = ExamSerializer
+    queryset = Exam.objects.all()  # type: ignore
+
+
+class ExamRetrieveUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ExamSerializer
+    queryset = Exam.objects.all()  # type: ignore
     lookup_field = "id"
