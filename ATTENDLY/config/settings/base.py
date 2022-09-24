d2a36bd737c955b1b2762d5b947014ac94b9c96d
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-24 12:50:32 +0000

    Register rest_framework application and add urls.
    Include the urls module of the students app.

--- ATTENDLY/config/settings/base.py
+++ ATTENDLY/config/settings/base.py
@@ -18,6 +18,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third party apps
+    "rest_framework",
     # Developer installed apps
     "users.apps.UsersConfig",
     "attendances.apps.AttendancesConfig",
