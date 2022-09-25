96e744934766b398cff25dc6276fcf126f693d6b
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-25 10:42:45 +0000

    Add fingerprint model

--- fingerprints/models.py
+++ fingerprints/models.py
@@ -1,3 +1,29 @@
+import uuid
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Fingerprint(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    created = models.DateTimeField(
+        verbose_name=_("Created"),
+        null=False,
+        blank=False,
+        auto_now_add=True,
+    )
+    file = models.FileField(
+        verbose_name=_("Template File"),
+        upload_to="fingerprints/",
+        blank=False,
+        null=False,
+    )
+    binary = models.BinaryField()
+
+    def __str__(self):
+        return f"{self.id}"
