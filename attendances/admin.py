d719ff15d8e2af7776d28de4b1ed9caacd851112
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-29 15:06:10 +0000

    Admin for exam attendance

--- attendances/admin.py
+++ attendances/admin.py
@@ -4,20 +4,25 @@ from django.contrib import admin
 from .models import ExamAttendance, StudentAttendance
 
 
+class StudentAttendanceInline(admin.TabularInline):
+    model = StudentAttendance
+
+
 @admin.register(ExamAttendance)
 class ExamAttendanceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'created', 'exam', 'room')
-    list_filter = ('created', 'exam', 'room')
+    list_display = ("id", "created", "exam", "room")
+    list_filter = ("created", "exam", "room")
+    inlines = [StudentAttendanceInline]
 
 
 @admin.register(StudentAttendance)
 class StudentAttendanceAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
-        'created',
-        'student',
-        'exam',
-        'room',
-        'exam_attendance',
+        "id",
+        "created",
+        "student",
+        "exam",
+        "room",
+        "exam_attendance",
     )
-    list_filter = ('created', 'student', 'exam', 'room', 'exam_attendance')
+    list_filter = ("created", "student", "exam", "room", "exam_attendance")
